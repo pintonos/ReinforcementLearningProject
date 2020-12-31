c19f6ef9cfe4956c7bc65ffb4e2a1b99bfd0907f
Josef Gugglberger <j.gugglberger@gmail.com>
2020-12-31 14:18:22 +0100

    change for acrobot gym envorinment add cartpole training logs

--- DQN/main.py
+++ DQN/main.py
@@ -15,20 +15,22 @@ argparser.add_argument('--train', dest='is_train', action='store_true')
 argparser.add_argument('--test', dest='is_train', action='store_false')
 argparser.set_defaults(is_train=True)
 
-argparser.add_argument("--model", default="./model/cartpole-dqn.h5", type=str, 
+argparser.add_argument("--model", default="./model/dqn-acrobot.h5", type=str, 
   help="load pretrained weights")
-argparser.add_argument("--log_file", default="./model/log.out", type=str, 
+argparser.add_argument("--log_file", default="./model/dqn-acrobot.out", type=str, 
   help="log file")
 
-argparser.add_argument("--save_interval", default=10, type=int, 
+argparser.add_argument("--save_interval", default=50, type=int, 
   help="interval of saving models, default: 10 episodes")
-argparser.add_argument("--env", default="CartPole-v1", type=str, 
+argparser.add_argument("--env", default="Acrobot-v1", type=str, 
   help="OpenAI gym environment")
 
 # TRAINING HYPERPARAMETERS
-argparser.add_argument("--episodes", default=1000, type=int, 
+argparser.add_argument("--episodes", default=500, type=int, 
   help="number of episodes")
-argparser.add_argument("--punishment", default=-10, type=int, 
+argparser.add_argument("--max_steps", default=500, type=int, 
+  help="max. number of steps per episode")
+argparser.add_argument("--punishment", default=-1, type=int, 
   help="negative reward on failure")
 argparser.add_argument("--batch_size", default=32, type=int, 
   help="minibatch size for replay buffer")
@@ -41,30 +43,35 @@ state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
 
 # init agent, load weights
-agent = DDQNAgent(state_size, action_size, args)
+agent = DQNAgent(state_size, action_size, args)
 if os.path.exists(args.model):
   agent.load(args.model)
 
 utils.log(args.log_file, 'step,score')
 
 # play the game
-done = False
 for episode in range(args.episodes):
     state = env.reset() 
     state = np.reshape(state, [1, state_size])
-    for step in range(500):
+
+    done = False
+    step = 0
+    sum_reward = 0
+    while not done and step < args.max_steps:
         # env.render()
         action = agent.act(state)
         next_state, reward, done, _ = env.step(action)
         reward = reward if not done else args.punishment
+        sum_reward += reward
         next_state = np.reshape(next_state, [1, state_size])
         agent.memorize(state, action, reward, next_state, done)
         state = next_state
-        if done:
-            utils.log(args.log_file, str(episode) + ',' + str(step))
-            break
         if len(agent.memory) > args.batch_size:
             agent.replay()
+        step += 1
+
+    utils.log(args.log_file, str(episode) + ',' + str(sum_reward))
+
     if args.is_train and episode % args.save_interval == 0:
         agent.save(args.model)
 
--- DQN/plot_csv.py
+++ DQN/plot_csv.py
@@ -7,15 +7,21 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-csv_in_path = 'model/log.out'
-png_out_path = 'dqnagent.png'
+csv_in_path = 'model/dqn-cartpole-v1.out'
+png_out_path = 'model/dqnagent.png'
 
 data = pd.read_csv(csv_in_path)
 
 plt.scatter(data['step'], data['score'])
-plt.title('Scatter plot of ' + csv_in_path)
+plt.title('Cartpole-v1')
 plt.xlabel('step')
 plt.ylabel('score')
+
+plt.axhline(y = 195, color = 'r', linestyle = '-') 
+
+# print avg. score of last 100 episodes
+last_100 = data["score"][-100:]
+print('avg score of last 100 episodes:', sum(last_100)/100)
 
 plt.savefig(png_out_path)
 plt.close()
